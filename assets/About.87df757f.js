fc02da517da5966f2314de142743c831529fa949
hathuytien <thuytienha127@gmail.com>
2022-05-27 21:36:05 +0700

    a

---
+++ assets/About.87df757f.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./index.873b53a1.js";import{o,a as s,d as a}from"./vendor.75ab2dbf.js";const t={},c={class:"about"},n=a("h1",null,"This is an about pagea",-1),r=[n];function _(d,i){return o(),s("div",c,r)}var u=e(t,[["render",_]]);export{u as default};
