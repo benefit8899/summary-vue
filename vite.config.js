336bb41ab18db6c6f67bbe7b21522b2289423c8a
hathuytien <thuytienha127@gmail.com>
2022-05-28 15:31:27 +0700

    av

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import vue from '@vitejs/plugin-vue'
 const path = require('path');
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/summary-vue/",
+  // base: "/summary-vue/",
   resolve: {
     alias: [
       {
