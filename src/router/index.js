827f976830e819e13bd060e4db3af27555ebf2b7
hathuytien <thuytienha127@gmail.com>
2022-05-28 14:23:40 +0700

    a

---
+++ src/router/index.js
@@ -0,0 +1,32 @@
+import { createRouter, createWebHistory } from "vue-router";
+import Home from "@/views/Home.vue";
+import Jewelry from "@/views/Jewelry.vue";
+
+const routes = [
+  {
+    path: "/summary-vue",
+    name: "Home",
+    component: Home,
+  },
+  {
+    path: "/summary-vue/about",
+    name: "About",
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+      import(/* webpackChunkName: "about" */ "@/views/About.vue"),
+  },
+  {
+    path: "/summary-vue/jewelry",
+    name: "Jewelry",
+    component: Jewelry
+  },
+];
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+});
+
+export default router;
