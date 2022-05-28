32208dca8f2d993d7cbe012506f32483228c381b
hathuytien <thuytienha127@gmail.com>
2022-05-28 16:11:50 +0700

    as

--- src/router/index.js
+++ src/router/index.js
@@ -18,7 +18,7 @@ const routes = [
       import(/* webpackChunkName: "about" */ "@/views/About.vue"),
   },
   {
-    path: "/jewelry",
+    path: "/summary-vue/jewelry",
     name: "Jewelry",
     component: Jewelry
   },
